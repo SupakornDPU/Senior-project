8496b0b0b69c4d631a2ceecddb9887973c8f48fb
BookThada <120725755+BookThada@users.noreply.github.com>
2023-11-24 18:12:26 +0700

    API POST Qus&Ans in textarea

--- public/js/manageFlashcard.js
+++ public/js/manageFlashcard.js
@@ -12,13 +12,54 @@ formImport.addEventListener('submit', (e) => {
    const formData = new FormData();
    formData.append('file', fileInput.files[0]);
 
-   fetch('/projectsenior/flashcard/import/' + deckId , {
-         method: 'POST',
-         body: formData
-      })
+   fetch('/projectsenior/flashcard/import/' + deckId, {
+      method: 'POST',
+      body: formData
+   })
       .then(res => res.json())
       .then(data => {
          console.log(data);
          // console.log(deckId);
       });
+});
+
+const formImports = document.getElementById('importQA');
+formImports.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   // ดึงข้อมูลจาก textarea ทั้งหมด
+   const questions = document.querySelectorAll('textarea[name="question"]');
+   const answers = document.querySelectorAll('textarea[name="answer"]');
+   const deckids = deckId;
+   const data = [];
+
+   // สร้างอาเรย์ของอ็อบเจ็กต์เพื่อเก็บข้อมูลจากทุก textarea
+   questions.forEach((question, index) => {
+      console.log(deckids);
+      data.push({
+         question: question.value,
+         answer: answers[index].value,
+         deckid : deckids
+      });
+   });
+   console.log(data)
+   // ทำ Fetch เพื่อส่งข้อมูลไปยังเซิร์ฟเวอร์ Node.js
+   fetch('/projectsenior/flashcard/' + deckId, {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ data })
+   })
+      .then(response => response.json())
+      .then(result =>{
+         console.log(result);
+
+         // Reset textarea values
+         questions.forEach(question => (question.value = ''));
+         answers.forEach(answer => (answer.value = ''));
+
+         alert("เพิ่มสำเร็จ");
+      })
+      .catch(err => console.log(err));
 });
