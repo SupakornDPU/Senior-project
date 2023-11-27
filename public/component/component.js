cb0f77707eb873271a1110b8bfc2c34f3c782bd1
Supakorn.Film <644607030002@dpu.ac.th>
2023-11-27 13:42:13 +0700

    edit file home to index

--- public/component/component.js
+++ public/component/component.js
@@ -18,7 +18,7 @@ class NavBar extends HTMLElement {
       <!-- Navbar -->
       <nav class="navbar navbar-expand-lg sticky-top font-poppin navbar-dark bg-dark">
       <div class="container">
-         <a class="navbar-brand" href="home">PyFlash</a>
+         <a class="navbar-brand" href="index">PyFlash</a>
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
             aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -26,7 +26,7 @@ class NavBar extends HTMLElement {
          <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="linkPage">
                <li class="nav-item">
-                  <a class="nav-link " aria-current="page" href="home">Home</a>
+                  <a class="nav-link " aria-current="page" href="index">Home</a>
                </li>
                <li class="nav-item">
                   <a class="nav-link " aria-current="page" href="#" id="menuClassroom">Classroom</a>
