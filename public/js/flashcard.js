7bdacfcafd7c8f30a916c31d1953ae4998151a1b
Supakorn.Film <644607030002@dpu.ac.th>
2023-11-29 17:17:46 +0700

    Update flashcard.js

--- public/js/flashcard.js
+++ public/js/flashcard.js
@@ -37,7 +37,7 @@ fetch('/projectsenior/deck/getById/' + deckID, {
             deckCol.innerHTML = `
             <div class="row justify-content-end">
                 <div class="col-3" style="width: fit-content;" ">
-                    <a id=" btnnextquestion" class="button btn btn-lg btn-next">
+                    <a id="btnnextquestion" class="button btn btn-lg btn-next">
                     <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 320 512">
                         <path
                             d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
@@ -51,7 +51,7 @@ fetch('/projectsenior/deck/getById/' + deckID, {
                     </h1>
                 </div>
                 <div class="col-md-12">
-                    <pre class="font-poppin d-flex justify-content-center" id="answer"
+                    <pre class="font-poppin d-flex justify-content-center" id="question"
                     style="font-weight: 500;padding-bottom: 20px;">
                         <code class="align-items-center">
 ${Item.card_question}
